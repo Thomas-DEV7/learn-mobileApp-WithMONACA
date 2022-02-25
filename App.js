e5387a8ac80f5d3dfa2c84668506b4f9dccf7b72
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-25 18:43:05 -0300

    Inputs e botoes

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá rapazes!</Text>
-      <Text>Olá rapazes!</Text>
+       <View style={styles.bloco}>
+          <Text style={styles.texto}>Valor 1</Text>
+          <TextInput style={styles.input} placeholder={'Digite o valor 1'}/>
+       </View>
+       <View style={styles.bloco}>
+          <Text style={styles.texto}>Valor 2</Text>
+          <TextInput style={styles.input} placeholder={'Digite o valor 2'}/>
+          <TouchableOpacity style={styles.botao}>
+         <Text>Entrar</Text>
+       </TouchableOpacity>
+       </View>
+       
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +29,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'arial'
   },
+  bloco:{
+    textAlign: 'center',
+    fontFamily: 'arial',
+  },
+  input: {
+    color: '#000',
+    fontSize: 25,
+    width: '100%',
+    borderColor: '#004',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop:30
+  },
+  texto:{
+    fontSize:20
+  },
+  botao:{
+    width:'80%', 
+    backgroundColor: '#400'
+    
+  }
 });
